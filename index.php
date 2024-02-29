ded75c24309ba3533b4e6cd94d9f617c92f60ef7
FatSassin <hubert@talwik.com>
2024-02-29 11:46:34 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bolyston Club</title>
+		<meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Morbius</h1>
+        <h2>Amongus</h2>
+        <h3>Garten Of BanBan</h3>
+        <br>
+        <?php
+           $x = 12;
+           $y = 13;
+           echo "Zawsze jem $x i $y jak jestem hungary"
+        ?>
+    </body>
+</html>
