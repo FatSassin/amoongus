de0dc76293830053c012604606ee7d2dc986b294
FatSassin <hubert@talwik.com>
2024-03-05 12:02:16 +0100

    cool

--- pupa.php
+++ pupa.php
@@ -8,18 +8,37 @@
         <form method="post">
             <input type='number' name="liczba" value="liczba1" />
             <input type='number' name="2liczba" value="liczba2" />
+            <input type='number' name="3liczba" value="liczba3" />
+            <input type='number' name="4liczba" value="liczba4" />
             <input type="submit" name="go" value="Submit" />
         </form>
-        <?php  
+        <?php 
+            /* 
               $a = $_POST['liczba'];
               $b = $_POST['2liczba'];
-              if ($b == 0){
+              $c = $_POST['3liczba'];
+              $d = $_POST['4liczba'];
+              if ($b == 0 or $d == 0){
                 echo "no can do";
               }
               else{
-                $x = $a/$b;
+                $x = $a/$b + $c/$d;
                 echo $x;
               };
+            */
+
+            /*
+            $a = $_POST['liczba'];
+            $b = $_POST['2liczba'];
+            if ($b-4 == 0){
+                echo "nuh uh";
+            }
+            else{
+            $x = ($a+6)/($b-4);
+            echo $x;
+            }
+            */
+
               
             /*
             $wiek = -1;
@@ -38,5 +57,59 @@
             }
             echo " <br>$x";
             */
+
+            /*
+            $a = $_POST['liczba'];
+            if ($a%2 == 0){
+                echo "parysta";
+            }
+            else {
+                echo "nieparista";
+            }
+            */
+
+            /*
+            $a = $_POST['liczba'];
+            $b = $_POST['2liczba'];
+            $x = $a/$b;
+            if ($a%$b == 0){
+                echo "podzielne, wynik ". $x;
+            }
+            else {
+                echo "niepowdzielne";
+            }
+            */
+
+            /*
+            $a = $_POST['liczba'];
+            if ($a>0){
+                echo "dodatnia";
+            }
+            elseif ($a<0){
+                echo "ujemna";
+            }
+            else {
+                echo "rowne zero";
+            }
+            */
+
+            $a = $_POST['liczba'];
+            $b = $_POST['2liczba'];
+            $c = $_POST['3liczba'];
+            if ($a>$b and $a>$c){
+                echo "liczba ". $a ." jest najwieksza";
+            }
+            elseif ($b>$a and $b>$c){
+                echo "liczba ". $b ." jest najwieksza";
+            }
+            elseif ($c>$a and $c>$b){
+                echo "liczba ". $c ." jest najwieksza";
+            }
+            else {
+                echo "nie ma najwiekszej lcizby";
+            }
+
+
+
         ?>
     </body>
