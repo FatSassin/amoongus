c8afd216d5df656410d70c06b10f517e40478edc
FatSassin <hubert@talwik.com>
2024-03-19 10:57:26 +0100

    gupie

--- kaka.php
+++ kaka.php
@@ -5,5 +5,42 @@
 		<meta charset="utf-8">
     </head>
     <body> 
+        <form method="post" >
+            <label>Liczba 1</label>
+            <input type="number" name="x">
+            <br>
+            <label>Liczba 2</label>
+            <input type="number" name="y">
+            <br>
+            <label>Liczba 3</label>
+            <input type="number" name="z">
+            <br>
+            <input type="submit" name="sus" value="submit">
+            <br>
+        </form>
     </body>
+    <?php
+    if (isset( $_POST['x'])){
+        $x = $_POST['x'];
+    }
+    if (isset( $_POST['y'])){
+        $y = $_POST['y'];
+    }
+    if (isset( $_POST['z'])){
+        $z = $_POST['z'];
+    }
+    function morbius($x, $y, $z)
+        {
+            if ($x>$y and $x>$z){
+                echo $x;
+            }
+            elseif ($y>$x and $y>$z){
+                echo $y;
+            }
+            else {
+                echo $z;
+            }
+        }
+    morbius($x,$y,$z)
+    ?>
 </html>
